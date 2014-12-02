aa101667d43babb7ca68d98b3cf4870234eab8f3
Marsfan <Lockheedmaniac@gmail.com>
2014-12-02 12:07:04 -0600

    Added code that MIGHT allows two power options.

--- admot.cpp
+++ admot.cpp
@@ -20,12 +20,16 @@ void Admot::mot2(int dir, int pwr)
 	analogWrite(11, pwr);
 }
 
-void Admot::both(int dir1, int dir2, int pwr)
+void Admot::both(int dir1, int dir2, int pwr1, int pwr2)
 {
 	digitalWrite(12, dir1);
 	digitalWrite(13, dir2);
-	analogWrite(3, pwr);
-	analogWrite(11, pwr);
+	analogWrite(3, pwr1);
+        if(pwr2 == null){
+                analogWrite(11, pwr1);
+       }else{  
+                analogWrite(11, pwr2);
+       }
 }
 
 void Admot::stop()
