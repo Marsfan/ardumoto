536b91aaca5a5b76b6ce7bc689ce9e1345493e23
Marsfan <marsfan@users.noreply.github.com>
2014-12-02 20:25:46 -0600

    fixed admot::both

--- admot.cpp
+++ admot.cpp
@@ -20,13 +20,13 @@ void Admot::mot2(int dir, int pwr)
 	analogWrite(11, pwr);
 }
 
-void Admot::both(int dir1, int dir2, int pwr1, int pwr2)
+void Admot::both(int dir1, int pwr1, int dir2, int pwr2)
 {
 	digitalWrite(12, dir1);
 	if(dir2 == -1){
-		digitalWrite(13, dir1);
+		digitalWrite(13, dir2);
 	}else{
-	digitalWrite(13, dir2);
+		digitalWrite(13, dir2);
 	}
 	analogWrite(3, pwr1);
         if(pwr2 == -1){
