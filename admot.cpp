3bf653fa862f99b65227a59d4bc50dacb93d2a67
Marsfan <marsfan@users.noreply.github.com>
2014-12-02 20:13:35 -0600

    updated code so that in admot::both, the 2nd directon is optional

--- admot.cpp
+++ admot.cpp
@@ -23,7 +23,11 @@ void Admot::mot2(int dir, int pwr)
 void Admot::both(int dir1, int dir2, int pwr1, int pwr2)
 {
 	digitalWrite(12, dir1);
+	if(dir2 == -1){
+		digitalWrite(13, dir1);
+	}else{
 	digitalWrite(13, dir2);
+	}
 	analogWrite(3, pwr1);
         if(pwr2 == -1){
                 analogWrite(11, pwr1);
